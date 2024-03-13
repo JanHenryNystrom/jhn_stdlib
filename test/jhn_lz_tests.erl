d5154db5d5a40eb9681440327ca7cb306a752d74
Henry Nystrom <JanHenryNystrom@gmail.com>
2024-03-13 23:35:52 +0100

    Added more tests for lz78 and lzw.

---
+++ test/jhn_lz_tests.erl
@@ -0,0 +1,73 @@
+%%==============================================================================
+%% Copyright 2024 Jan Henry Nystrom <JanHenryNystrom@gmail.com>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%==============================================================================
+
+%%%-------------------------------------------------------------------
+%%% @doc
+%%%   eunit unit tests for the jhn_lz library module.
+%%% @end
+%%%
+%% @author Jan Henry Nystrom <JanHenryNystrom@gmail.com>
+%% @copyright (C) 2024, Jan Henry Nystrom <JanHenryNystrom@gmail.com>
+%%%-------------------------------------------------------------------
+-module(jhn_lz_tests).
+-copyright('Jan Henry Nystrom <JanHenryNystrom@gmail.com>').
+
+%% Includes
+-include_lib("eunit/include/eunit.hrl").
+
+%% Defines
+-define(TEXTS, [<<"TOBEORNOTTOBE">>,
+                <<"YABBADABBADABBADOO">>,
+                <<"AAAAAAAAAAAAAAAAAA">>]).
+
+%% ===================================================================
+%% Tests.
+%% ===================================================================
+
+%% foo_test_() ->
+%%     [?_test(?assertEqual(xx, rfc(2732)))].
+
+%%--------------------------------------------------------------------
+%% lz78_compress/1 <-> lz78w_uncompress/1
+%%--------------------------------------------------------------------
+lz78_compress_1_lz78w_uncompress_2_test_() ->
+    [?_test(
+        ?assertEqual(T,
+                     jhn_lz:lz78w_uncompress(jhn_lz:lz78_compress(T),
+                                             [binary]) )) ||
+        T <- [rfc(2732), rfc(2818) | ?TEXTS]
+    ].
+
+%%--------------------------------------------------------------------
+%% lzw_compress/1 <-> lz78w_uncompress/1
+%%--------------------------------------------------------------------
+lzw_compress_1_lz78w_uncompress_2_test_() ->
+    [?_test(
+        ?assertEqual(T,
+                     jhn_lz:lz78w_uncompress(jhn_lz:lzw_compress(T),
+                                             [binary]) )) ||
+        T <- [rfc(2732), rfc(2818) | ?TEXTS]
+    ].
+
+%% ===================================================================
+%% Internal functions.
+%% ===================================================================
+
+rfc(Int) ->
+    Dir = code:lib_dir(jhn_stdlib, test),
+    FileName = "rfc" ++ integer_to_list(Int) ++ ".txt",
+    {ok, File} = file:read_file(filename:join([Dir, "jhn_lz", FileName])),
+    File.
