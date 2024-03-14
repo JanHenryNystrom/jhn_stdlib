f669db8ea3bb4c6194ebe86b5bde7d64d333ceb9
Henry Nystrom <JanHenryNystrom@gmail.com>
2024-03-14 10:39:17 +0100

    More tests.

--- test/jhn_lz_tests.erl
+++ test/jhn_lz_tests.erl
@@ -43,27 +43,73 @@
 %%--------------------------------------------------------------------
 %% lz78_compress/1 <-> lz78w_uncompress/1
 %%--------------------------------------------------------------------
+lz78_compress_1_lz78w_uncompress_1_test_() ->
+    [?_test(
+        ?assertEqual(T,
+                     iolist_to_binary(
+                       jhn_lz:lz78w_uncompress(jhn_lz:lz78_compress(T))))) ||
+        T <- [rfc(2732), rfc(2818) | ?TEXTS]
+    ].
+
+%%--------------------------------------------------------------------
+%% lz78_compress/1 <-> lz78w_uncompress/2
+%%--------------------------------------------------------------------
 lz78_compress_1_lz78w_uncompress_2_test_() ->
     [?_test(
         ?assertEqual(T,
                      jhn_lz:lz78w_uncompress(jhn_lz:lz78_compress(T),
-                                             [binary]) )) ||
+                                             [binary]))) ||
+        T <- [rfc(2732), rfc(2818) | ?TEXTS]
+    ] ++
+        [?_test(
+            ?assertEqual(T,
+                         iolist_to_binary(
+                           jhn_lz:lz78w_uncompress(jhn_lz:lz78_compress(T),
+                                                   [iolist])))) ||
+            T <- [rfc(2732), rfc(2818) | ?TEXTS]
+        ].
+
+%%--------------------------------------------------------------------
+%% lz78_init/0 lz78w_cont/2 lz78w_end/1 <-> lz78w_uncompress/2
+%%--------------------------------------------------------------------
+lz78_init_0_lz78w_cont_2_lz78w_end_1_lz78w_uncompress_2_test_() ->
+    [?_test(
+        ?assertEqual(T,
+                     jhn_lz:lz78w_uncompress(
+                       jhn_lz:lz78w_end(
+                         jhn_lz:lz78w_cont(T, jhn_lz:lz78_init())),
+                       [binary]))) ||
         T <- [rfc(2732), rfc(2818) | ?TEXTS]
     ].
 
 %%--------------------------------------------------------------------
-%% lzw_compress/1 <-> lz78w_uncompress/1
+%% lz78_init/0 lz78w_cont/2 lz78w_end/2 <-> lz78w_uncompress/2
+%%--------------------------------------------------------------------
+lz78_init_0_lz78w_cont_2_lz78w_end_2_lz78w_uncompress_2_test_() ->
+    [?_test(
+        ?assertEqual(<<T/binary, T/binary>>,
+                     jhn_lz:lz78w_uncompress(
+                       jhn_lz:lz78w_end(
+                         T,
+                         jhn_lz:lz78w_cont(T, jhn_lz:lz78_init())),
+                       [binary]))) ||
+        T <- [rfc(2732), rfc(2818) | ?TEXTS]
+    ].
+
+%%--------------------------------------------------------------------
+%% lzw_compress/1 <-> lz78w_uncompress/2
 %%--------------------------------------------------------------------
 lzw_compress_1_lz78w_uncompress_2_test_() ->
     [?_test(
         ?assertEqual(T,
-                     jhn_lz:lz78w_uncompress(jhn_lz:lzw_compress(T),
-                                             [binary]) )) ||
+                     jhn_lz:lz78w_uncompress(
+                       jhn_lz:lzw_compress(T),
+                       [binary]))) ||
         T <- [rfc(2732), rfc(2818) | ?TEXTS]
     ].
 
 %%--------------------------------------------------------------------
-%% lzw_compress/2 <-> lz78w_uncompress/1
+%% lzw_compress/2 <-> lz78w_uncompress/2
 %%--------------------------------------------------------------------
 lzw_compress_2_lz78w_uncompress_2_test_() ->
     Dict = lists:foldl(fun(C, Acc) -> maps:put(<<C:8/integer>>, C, Acc) end,
@@ -71,8 +117,9 @@ lzw_compress_2_lz78w_uncompress_2_test_() ->
                        lists:seq(32, 127)),
     [?_test(
         ?assertEqual(T,
-                     jhn_lz:lz78w_uncompress(jhn_lz:lzw_compress(T, Dict),
-                                             [binary]) )) ||
+                     jhn_lz:lz78w_uncompress(
+                       jhn_lz:lzw_compress(T, Dict),
+                       [binary]))) ||
         T <- [rfc(2732), rfc(2818) | ?TEXTS]
     ].
 
